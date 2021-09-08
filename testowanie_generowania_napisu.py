d85e0341b077ca821a446063221f8eb1d416b1e4
kacper-ziemecki <kacperziemecki@o2.pl>
2021-09-08 16:44:08 +0200

    testowanie generacji hasla

---
+++ testowanie_generowania_napisu.py
@@ -0,0 +1,18 @@
+def wpisz_haslo(length, lista = [], danyNapis = "", dlugosc = 0):
+	dlugosc += 1
+
+	if(dlugosc > length):
+		lista.append(danyNapis)
+
+		return lista
+
+	for znak in listaznakow:
+		nastepnyNapis = danyNapis[:]
+		nastepnyNapis += znak 
+		wpisz_haslo(length, lista, nastepnyNapis, dlugosc)
+
+	return lista
+
+lista = wpisz_haslo(3)
+for element in lista:
+  print(element)
