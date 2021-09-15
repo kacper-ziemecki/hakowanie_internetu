a73df0693c81cfa3d5c830082de4a8adf3825102
kacper-ziemecki <kacperziemecki@o2.pl>
2021-09-15 19:38:28 +0200

    powodzenie w testowaniu driverów do chrome

--- testowanie_edge_driver.py
+++ testowanie_edge_driver.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 
 # options = webdriver.EdgeOptions()
 # options.use_chromium = True
-objektStronyEdge = webdriver.Edge("https://docs.microsoft.com/en-us/microsoft-edge/webdriver-chromium/?tabs=python") #wpisać randomową stronę internetową
+objektStronyChrome = webdriver.Chrome(r"C:\Users\kacpe\Documents\Programowanie\projekty_programowanie\hakowanie_internetu\chromedriver_win32\chromedriver.exe") 
+objektStronyChrome.get(r"https://chromedriver.chromium.org/getting-started")
 print("w tym momencie strona powinna być uruchomiona w edge")
 input("naciśnij klawisz enter aby zakończyć program")
